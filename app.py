448c59cc9d358b112ab208fcf85ca28244c03365
Eric VanBuhler <eric@alwaysai.co>
2021-03-23 13:42:37 -0700

    Include boxes and labels in captured image

--- app.py
+++ app.py
@@ -3,29 +3,27 @@ import edgeiq
 import cv2
 
 
-new_people_detected = []
-
-
-def person_enters(person_id, prediction):
-    print('Person {} enters'.format(person_id))
-    global new_people_detected
-    new_people_detected.append(person_id)
-
-
-def person_exits(person_id, prediction):
-    print('Person {} exits'.format(person_id))
-
-
-def save_snapshot(image, person_id):
+def save_snapshot(image):
     snap_date = time.strftime('%Y.%m.%d')
     snap_time = time.strftime('%H.%M.%S')
-    filename = '{}-{}-person-{}.jpg'.format(snap_date, snap_time, person_id)
+    filename = '{}-{}.jpg'.format(snap_date, snap_time)
     cv2.imwrite(filename, image)
 
 
 def main():
     obj_detect = edgeiq.ObjectDetection("alwaysai/mobilenet_ssd")
     obj_detect.load(engine=edgeiq.Engine.DNN)
+
+    new_detection = False
+
+    def person_enters(person_id, prediction):
+        print('Person {} enters'.format(person_id))
+        nonlocal new_detection
+        new_detection = True
+
+    def person_exits(person_id, prediction):
+        print('Person {} exits'.format(person_id))
+
     tracker = edgeiq.CentroidTracker(
             deregister_frames=30, enter_cb=person_enters, exit_cb=person_exits)
 
@@ -50,11 +48,6 @@ def main():
                         results.predictions, ['person'])
                 tracked_people = tracker.update(people)
 
-                global new_people_detected
-                for person in new_people_detected:
-                    save_snapshot(frame, person)
-                new_people_detected = []
-
                 people = []
                 for (object_id, prediction) in tracked_people.items():
                     new_label = 'Person {}'.format(object_id)
@@ -63,6 +56,10 @@ def main():
 
                 frame = edgeiq.markup_image(
                         frame, people, colors=obj_detect.colors)
+
+                if new_detection:
+                    save_snapshot(frame)
+                new_detection = False
 
                 # Generate text to display on streamer
                 text = ["Model: {}".format(obj_detect.model_id)]
