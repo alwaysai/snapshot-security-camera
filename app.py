2172cad572f9a00a39e57a8947f394d5121782fb
Eric VanBuhler <eric@alwaysai.co>
2021-03-22 22:34:11 -0700

    Use enter and exit callbacks in new tracking release

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import time
 import edgeiq
 import cv2
-"""
-Use object detection to detect objects in the frame in realtime. The
-types of objects detected can be changed by selecting different models.
 
-To change the computer vision model, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
 
-To change the engine and accelerator, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_and_accelerator.html
-"""
+new_people_detected = []
+
+
+def person_enters(person_id, prediction):
+    print('Person {} enters'.format(person_id))
+    global new_people_detected
+    new_people_detected.append(person_id)
+
+
+def person_exits(person_id, prediction):
+    print('Person {} exits'.format(person_id))
 
 
 def save_snapshot(image, person_id):
@@ -21,10 +24,10 @@ def save_snapshot(image, person_id):
 
 
 def main():
-    obj_detect = edgeiq.ObjectDetection(
-            "alwaysai/mobilenet_ssd")
+    obj_detect = edgeiq.ObjectDetection("alwaysai/mobilenet_ssd")
     obj_detect.load(engine=edgeiq.Engine.DNN)
-    tracker = edgeiq.CentroidTracker(deregister_frames=30)
+    tracker = edgeiq.CentroidTracker(
+            deregister_frames=30, enter_cb=person_enters, exit_cb=person_exits)
 
     print("Loaded model:\n{}\n".format(obj_detect.model_id))
     print("Engine: {}".format(obj_detect.engine))
@@ -34,21 +37,23 @@ def main():
     fps = edgeiq.FPS()
 
     try:
-        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
+        with edgeiq.WebcamVideoStream(cam=2) as video_stream, \
                 edgeiq.Streamer() as streamer:
             # Allow Webcam to warm up
             time.sleep(2.0)
             fps.start()
 
-            prev_tracked_people = {}
-            logs = []
-
-            # loop detection
             while True:
                 frame = video_stream.read()
                 results = obj_detect.detect_objects(frame, confidence_level=.5)
-                people = edgeiq.filter_predictions_by_label(results.predictions, ['person'])
+                people = edgeiq.filter_predictions_by_label(
+                        results.predictions, ['person'])
                 tracked_people = tracker.update(people)
+
+                global new_people_detected
+                for person in new_people_detected:
+                    save_snapshot(frame, person)
+                new_people_detected = []
 
                 people = []
                 for (object_id, prediction) in tracked_people.items():
@@ -59,17 +64,6 @@ def main():
                 frame = edgeiq.markup_image(
                         frame, people, colors=obj_detect.colors)
 
-                new_entries = set(tracked_people) - set(prev_tracked_people)
-                for entry in new_entries:
-                    save_snapshot(frame, entry)
-                    logs.append('Person {} entered'.format(entry))
-
-                new_exits = set(prev_tracked_people) - set(tracked_people)
-                for exit in new_exits:
-                    logs.append('Person {} exited'.format(exit))
-
-                prev_tracked_people = dict(tracked_people)
-
                 # Generate text to display on streamer
                 text = ["Model: {}".format(obj_detect.model_id)]
                 text.append(
@@ -79,9 +73,6 @@ def main():
                 for prediction in people:
                     text.append("{}: {:2.2f}%".format(
                         prediction.label, prediction.confidence * 100))
-
-                text.append('Logs:')
-                text += logs
 
                 streamer.send_data(frame, text)
 
